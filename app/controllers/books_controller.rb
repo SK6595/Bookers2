7dec178798a91f3c3c26924d50e4a8bdb21c11d6
SK6595 <173334721+SK6595@users.noreply.github.com>
2024-09-03 08:30:06 +0000

    Userのshowページの作成まで終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,8 @@
 class BooksController < ApplicationController
-  
-  def new
-    @book = Book.new
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
-  end
-  
+
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     @book.save
     redirect_to book_path(@book.id)
   end
@@ -16,19 +10,35 @@ class BooksController < ApplicationController
   def index
     @user = current_user
     @book = Book.new
+    @books = Book.all
   end
 
   def show
-    @book = Book.new
+    @book_new = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+
+  def edit
+    @book = Book.find(params[:id])
   end
   
-  def edit
-  end 
-    
+  def update
+   book = Book.find(params[:id])
+   book.update(book_params)
+   redirect_to book_path(book.id)  
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
 
   def book_params
-    params.require(:book).permit(:title, :image, :body)
+    params.require(:book).permit(:title,:body)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   root to: "homes#top"
 
   get 'home/about' => 'homes#about'
-  
-  post 'books' => 'books#create'
+
+  patch 'books/:id' => 'books#update', as: 'update_book'
 
   resources :users, only: [:index, :show, :edit, :update]
 
-  resources :books, only: [:new, :create, :index, :show]
+  resources :books, only: [:create, :index, :show , :edit, :update, :destroy]
 
 end
