d66e2a6ff438152490809fba7e748cfd54b1ec55
SK6595 <173334721+SK6595@users.noreply.github.com>
2024-09-05 08:04:30 +0000

    1 error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
 
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -15,7 +17,6 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
-    @user = current_user
     @books = Book.all
   end
 
@@ -28,7 +29,7 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
    @book = Book.find(params[:id])
    if @book.update(book_params)
@@ -38,7 +39,7 @@ class BooksController < ApplicationController
      render :edit
    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
@@ -49,6 +50,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title,:body)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
   end
 
 end
